4807f576668cda863578852cd671c800e95adce1
kennyhazzar <56494597+kennyhazzar@users.noreply.github.com>
2022-05-30 12:27:42 +0700

    remove methods: /link; /link/all. add trycatch for all methods in link controller

--- src/link/link.controller.ts
+++ src/link/link.controller.ts
@@ -1,7 +1,6 @@
 import {
   Controller,
   Get,
-  Ip,
   Param,
   Post,
   Query,
@@ -13,26 +12,29 @@ import { LinkService } from './link.service';
 
 @Controller('link')
 export class LinkController {
-  constructor(private linkService: LinkService) {}
-
-  @Get()
-  linkController(@Ip() ip): string {
-    return `main shit. your ip is ${ip}`;
-  }
+  constructor(private linkService: LinkService) { }
 
   @Post('/create')
-  createLink(@Query('link') link) {
-    return this.linkService.create(link);
-  }
-
-  @Get('/all')
-  getAll() {
-    return this.linkService.findAll();
+  createLink(@Query('link') link, @Res() response: Response) {
+    try {
+      return this.linkService.create(link);
+    } catch (error) {
+      return response.status(500).send({ error: `can't create link: something wrong in code` })
+    }
   }
 
   @Get('find')
-  findLink(@Query('title') title) {
-    return this.linkService.findLinksByTitle(title);
+  async findLink(@Query('title') title: string, @Res() response: Response) {
+    try {
+      const links = await this.linkService.findLinksByTitle(title);
+
+      if (!links || links.length === 0) {
+        return response.status(404).send({ error: `this links does not exist` })
+      }
+
+    } catch (error) {
+      response.status(500).send({ error: 'something wrong in server' });
+    }
   }
 
   @Get(':id')
